d61595d7e37396ab76af3263416d02165eca28e4
Noah Fajarda <noahfajarda@gmail.com>
2023-05-13 12:11:22 -0700

    implemented conditional navbar based on log in state

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,71 @@
-import React from "react";
+import React, { useContext } from "react";
 // Link == WILL PREVENT PAGE FROM REFRESHING!!
 import { Link } from "react-router-dom";
 // import capitalize first letter function
 import capitalizeFirstLetter from "../utils/capitalizeFirstLetter";
+import { UserContext } from "../App";
 
 export default function Navbar() {
-  const pages = ["login", "signup", "profile", "create"];
+  const { state, dispatch } = useContext(UserContext);
+
+  let loggedIn = state?._id ? true : undefined;
+
+  // pages and bool if shown if logged in
+  const pages = [
+    {
+      loggedIn: undefined,
+      page: "login",
+    },
+    {
+      loggedIn: undefined,
+      page: "signup",
+    },
+    {
+      loggedIn: true,
+      page: "profile",
+    },
+    {
+      loggedIn: true,
+      page: "create",
+    },
+  ];
 
   return (
     <nav>
       <div className="nav-wrapper">
         {/* Link == WON'T REFRESH PAGE */}
-        <Link to="/home" className="brand-logo left text-black">
+        <Link
+          // conditional for logo
+          to={state ? "/" : "/login"}
+          className="brand-logo left text-black"
+        >
           Instagram
         </Link>
         <ul id="nav-mobile" className="right">
-          {pages.map((page, idx) => (
-            <li key={idx}>
-              {/* iterate through pages array to add to navbar */}
-              <Link to={`/${page}`} className="text-black">
-                {capitalizeFirstLetter(page)}
-              </Link>
-            </li>
-          ))}
+          {/*  iterate through pages array to add to navbar  */}
+          {pages.map((page, idx) => {
+            // display based on if user is logged in or not
+            const bool = loggedIn === page.loggedIn;
+            if (bool) {
+              return renderPage(idx, page);
+            } else if (bool === undefined) {
+              return renderPage(idx, page);
+            }
+            return <div key={idx}></div>;
+          })}
         </ul>
       </div>
     </nav>
+  );
+}
+
+// render accesible nav element
+function renderPage(idx, page) {
+  return (
+    <li key={idx}>
+      <Link to={`/${page.page}`} className="text-black">
+        {capitalizeFirstLetter(page.page)}
+      </Link>
+    </li>
   );
 }
