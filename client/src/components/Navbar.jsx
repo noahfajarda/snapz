e931c5647f5a986742c477621ede15f7b729fa0f
Noah Fajarda <noahfajarda@gmail.com>
2023-05-23 10:38:53 -0700

    added search users option

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,38 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useRef, useEffect } from "react";
 // Link == WILL PREVENT PAGE FROM REFRESHING!!
 import { Link, useNavigate } from "react-router-dom";
 // import capitalize first letter function
 import capitalizeFirstLetter from "../utils/capitalizeFirstLetter";
 import { UserContext } from "../App";
+import M from "materialize-css";
 
 export default function Navbar() {
   const navigate = useNavigate();
   // retrieve user context data
   const { state, dispatch } = useContext(UserContext);
+  // search modal
+  const searchModal = useRef(null);
+  const [search, setSearch] = useState("");
+  const [userDetails, setUserDetails] = useState([]);
+
+  useEffect(() => {
+    M.Modal.init(searchModal.current);
+  }, []);
+
+  const fetchUsers = (query) => {
+    setSearch(query);
+    fetch("/search-users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then((res) => res.json())
+      .then((results) => {
+        setUserDetails(results);
+      });
+  };
 
   // if user context data object exists, user is logged in
   let loggedIn = state?._id ? true : undefined;
@@ -44,14 +68,28 @@ export default function Navbar() {
   return (
     <nav>
       <div className="nav-wrapper">
-        {/* Link == WON'T REFRESH PAGE */}
-        <Link
-          // conditional for logo
-          to={state ? "/" : "/login"}
-          className="brand-logo left text-black"
-        >
-          Instagram
-        </Link>
+        <div>
+          {/* Link == WON'T REFRESH PAGE */}
+          <Link
+            // conditional for logo
+            to={state ? "/" : "/login"}
+            className="brand-logo left text-black"
+          >
+            Instagram
+          </Link>
+          {/* unique for search */}
+          {loggedIn && (
+            <li key={6}>
+              <i
+                data-target="modal1"
+                className="large material-icons modal-trigger search-users"
+                style={{ color: "black" }}
+              >
+                search
+              </i>
+            </li>
+          )}
+        </div>
         <ul id="nav-mobile" className="right">
           {/*  iterate through pages array to add to navbar  */}
           {pages.map((page, idx) => {
@@ -65,6 +103,51 @@ export default function Navbar() {
             return <div key={idx}></div>;
           })}
         </ul>
+      </div>
+
+      {/* search modal */}
+      <div
+        id="modal1"
+        className="modal"
+        ref={searchModal}
+        style={{ color: "black" }}
+      >
+        <div className="modal-content">
+          <div className="modal-footer">
+            <button className="modal-close waves-effect waves-green btn-flat">
+              Close
+            </button>
+          </div>
+          <input
+            type="text"
+            placeholder="search users"
+            value={search}
+            className="text-black"
+            onChange={(e) => fetchUsers(e.target.value)}
+          />
+          <ul className="collection flex flex-col">
+            {userDetails?.user &&
+              userDetails?.user.map((item) => (
+                <Link
+                  key={item._id}
+                  to={
+                    item._id !== state._id ? `/profile/${item._id}` : "/profile"
+                  }
+                  onClick={async () => {
+                    M.Modal.getInstance(searchModal.current).close();
+                    setSearch("");
+
+                    // check for pathname
+                    window.location.pathname.startsWith("/profile/") &&
+                      navigate(`/profile/${item._id}`);
+                    window.location.reload();
+                  }}
+                >
+                  <li className="collection-item text-black">{item.email}</li>
+                </Link>
+              ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
