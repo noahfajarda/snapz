788b961f03b84cd2fb21a948f067e4f06369e63b
Noah Fajarda <noahfajarda@gmail.com>
2023-07-07 20:09:57 -0700

    removed focus on menu hamburger icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -161,8 +161,8 @@ export default function Navbar() {
         {/* mobile nav button */}
         <div className="flex mobile-nav-container">
           <div className="hamburger">Menu</div>
-          <button
-            className="hamburger"
+          <div
+            className="hamburger-icon"
             onClick={() => {
               setIsNavExpanded(!isNavExpanded);
             }}
@@ -177,7 +177,7 @@ export default function Navbar() {
             >
               <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
             </svg>
-          </button>
+          </div>
         </div>
       </section>
       {/* modal */}
